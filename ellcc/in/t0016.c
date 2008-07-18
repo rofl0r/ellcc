c13f67812a26b5a6d495b479505ecfea4b1233c4
Richard Pennington <rich@ellcc.org>
2008-07-18 20:39:15 +0000

    bug 134: Regressions and more during newlib incorporation.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@728 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ellcc/in/t0016.c
+++ ellcc/in/t0016.c
@@ -1,10 +1,6 @@
 // A floating point divide.
-    extern double tens[10];
 int main()
 {
-    int ilim;
     double a, b;
-    long l;
-    a = 0.5 / tens[ilim - 1] - b;
-    l = a / b;
+    a /= b;
 }
