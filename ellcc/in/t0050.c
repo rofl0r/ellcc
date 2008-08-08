8c5519c2ee3de6b3cd4cdc851445936b11129efe
Richard Pennington <rich@ellcc.org>
2008-08-08 15:19:57 +0000

    bug 134: Regressions and more when building newlib.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@771 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ellcc/in/t0050.c
+++ ellcc/in/t0050.c
@@ -1,25 +1,14 @@
-#if 1
-void print_number();
-#else
-void print_number(int base, int unsigned_p, long n);
-#endif
-
-void __dprintf (char *fmt, ...)
-{
-    char* p;
-    char c;
-    long l;
-
-    print_number (c /* == 'x' ? 16 : 10 */, c /* != 'd' */, l);
-}
-
-void print_number (base, unsigned_p, n)
- int base;
- int unsigned_p;
- long n;
-{
-}
+// K&R function argument promotion.
+void f();
 
 int main()
+{
+    char c;
+
+    f(c);
+}
+
+void f(i)
+    int i;
 {
 }
