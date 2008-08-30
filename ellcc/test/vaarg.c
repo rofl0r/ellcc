1f10c9bb8344e4a302c39349f5e8a5183eee935e
Richard Pennington <rich@ellcc.org>
2008-08-30 21:43:28 +0000

    bug 134: Regressions and more when building newlib.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@819 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ellcc/test/vaarg.c
+++ ellcc/test/vaarg.c
@@ -1,14 +1,14 @@
 #include <stdarg.h>
 void f(char* p, ...)
 {
+    va_list ap;
+    va_start(ap, p);
+    va_end(ap);
 }
 
 int main(void)
 {
     char* p;
     int i;
-    va_list ap;
-    // va_start(ap, p);
-    // f(p, i);
-    // va_end(ap);
+    f(p, i);
 }
