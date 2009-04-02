83cacfb98f7b56faaea4e4e403b96be6e58e3d34
Richard Pennington <rich@ellcc.org>
2009-04-02 12:06:23 +0000

    bug 162: Adding inline asm support.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@968 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ellcc/in/t0059.c
+++ ellcc/in/t0059.c
@@ -1,9 +1,9 @@
 double _f_exp (double x)
 {
- double result;
- asm ("fldl2e; fmulp; fld %%st; frndint; fsub %%st,%%st(1); fxch;"
-      "fchs; f2xm1; fld1; faddp; fxch; fld1; fscale; fstp %%st(1); fmulp"
-      : [res] "=t"(result)
-      : "[res]"(x));
- return result;
+   double result;
+   asm ("fldl2e; fmulp; fld %%st; frndint; fsub %%st,%%st(1); fxch;"
+        "fchs; f2xm1; fld1; faddp; fxch; fld1; fscale; fstp %%st(1); fmulp"
+        : [res] "=t"(result)
+        : "[res]"(x));
+   return result;
 }
