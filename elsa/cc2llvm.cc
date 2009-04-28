4ef8a78ec664052398888da73174ee8e75242abe
Richard Pennington <rich@ellcc.org>
2009-04-28 19:06:15 +0000

    bug 134: Regressions while importing newlib.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1030 82857169-6e38-49d7-aaa8-07fb558f12fc

--- elsa/cc2llvm.cc
+++ elsa/cc2llvm.cc
@@ -27,7 +27,7 @@ using namespace elsa;
 
 #define SRET 1
 
-#if 1
+#if 0
 // Really verbose debugging.
 #define VDEBUG(who, where, what) std::cerr << toString(where) << ": " << who << " "; what; std::cerr << "\n"
 #else
