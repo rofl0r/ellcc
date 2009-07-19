c83c4397efed89429e718a8f8a9564132b54e0b7
Richard Pennington <rich@ellcc.org>
2009-07-19 13:25:26 +0000

    bug 134: More changes for libecc.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1314 82857169-6e38-49d7-aaa8-07fb558f12fc

--- cmake/x86-pe.cmake
+++ cmake/x86-pe.cmake
@@ -4,8 +4,8 @@ INCLUDE(CMakeForceCompiler)
 SET(CMAKE_SYSTEM_NAME Generic)
 
 # which compilers to use for C and C++
-CMAKE_FORCE_C_COMPILER(/home/rich/ellcc/ellcc/arm-elf-ecc GNU)
-CMAKE_FORCE_CXX_COMPILER(/home/rich/ellcc/ellcc/arm-elf-e++ GNU)
+CMAKE_FORCE_C_COMPILER(/home/rich/ellcc/ellcc/x86-pe-ecc GNU)
+CMAKE_FORCE_CXX_COMPILER(/home/rich/ellcc/ellcc/x86-pe-e++ GNU)
 
 # here is the target environment is located
 SET(CMAKE_FIND_ROOT_PATH  /usr/rich/ellcc)
