a9bcda798b220463d599e9be7821fb24da5cfb39
Richard Pennington <rich@ellcc.org>
2009-07-21 14:02:41 +0000

    bug 134: More changes for libecc.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1321 82857169-6e38-49d7-aaa8-07fb558f12fc

--- cmake/target.cmake
+++ cmake/target.cmake
@@ -3,9 +3,12 @@ INCLUDE(CMakeForceCompiler)
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Generic)
 
+if(ELLCC_TARGET)
+    set(sep "-")
+endif(ELLCC_TARGET)
 # which compilers to use for C and C++
-CMAKE_FORCE_C_COMPILER(${ELLCC_SOURCE_DIR}/${ELLCC_TARGET}-ecc GNU)
-CMAKE_FORCE_CXX_COMPILER(${ELLCC_SOURCE_DIR}/${ELLCC_TARGET}-e++ GNU)
+CMAKE_FORCE_C_COMPILER(${ELLCC_SOURCE_DIR}/ellcc/${ELLCC_TARGET}${sep}ecc GNU)
+CMAKE_FORCE_CXX_COMPILER(${ELLCC_SOURCE_DIR}/ellcc/${ELLCC_TARGET}${sep}e++ GNU)
 
 # here is the target environment is located
 SET(CMAKE_FIND_ROOT_PATH  ${ELLCC_SOURCE_DIR})
