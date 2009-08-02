90c3ea6209d6893a2934e008fbd412fd308fd212
Richard Pennington <rich@ellcc.org>
2009-08-02 19:54:48 +0000

    bug 134: More changes for libecc.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1384 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/include/x86_64-elf/asm/_start.h
+++ libecc/include/x86_64-elf/asm/_start.h
@@ -2,22 +2,18 @@
  */
 asm(
 "       .text\n"
-"       .globl  _start\n"
-"       .align  2\n"
-"_start:\n"
-"       .text\n"
 "       .align  16\n"
 "       .globl  _start\n"
 "       .type   _start,@function\n"
 "_start:\n"
-"       xorl    %rbp, %rbp\n"           // Clear the link register.
-"       movl    (%rsp), %rax\n"         // Get argc...
-"       leal    4(%rsp), %rcx\n"        // ... and argv ...
-"       leal    (%rcx, %rax, 4), %rdx\n"// ... and compute environ.
-"       subl    $12, %rsp\n"            // Room for arguments.
-"       movl    %rdx, 8(%rsp)\n"
-"       movl    %rcx, 4(%rsp)\n"
-"       movl    %rax, (%rsp)\n"
+"       xor     %rbp, %rbp\n"           // Clear the link register.
+"       mov     (%rsp), %rax\n"         // Get argc...
+"       lea     4(%rsp), %rcx\n"        // ... and argv ...
+"       lea     (%rcx, %rax, 4), %rdx\n"// ... and compute environ.
+"       sub     $12, %rsp\n"            // Room for arguments.
+"       mov     %rdx, 8(%rsp)\n"
+"       mov     %rcx, 4(%rsp)\n"
+"       mov     %rax, (%rsp)\n"
 "       call    _estart\n"              // Let's go!
 "       jmp     .\n"                    // Never gets here.
 "       .size   _start, .-_start\n"
