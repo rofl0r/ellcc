e8d907c625a9b37d56a56c4b1470e9e604e7b42a
Richard Pennington <rich@ellcc.org>
2009-10-24 20:55:50 +0000

    Move ELLCC into a subdirectory.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1565 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ellcc/Elkhound/c/configure.pl
+++ ellcc/Elkhound/c/configure.pl
@@ -4,7 +4,7 @@
 use strict 'subs';
 
 # default location of smbase relative to this package
-$SMBASE = "../../smbase";
+$SMBASE = "../../SMBase";
 $req_smcv = 1.03;            # required sm_config version number
 $thisPackage = "elkhound/c";
 
@@ -41,7 +41,7 @@ if ($smcv < $req_smcv) {
 
 
 # defaults
-$AST = "../../ast";
+$AST = "../../Ast";
 $ELKHOUND = "..";
 
 
