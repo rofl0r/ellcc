0174c8a2845ca795d28f7287e87c54480da7a2fe
Richard Pennington <rich@ellcc.org>
2009-12-10 13:01:25 +0000

    Changes for state machine support.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1627 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ecc/Elsa/state.cc
+++ ecc/Elsa/state.cc
@@ -2,6 +2,7 @@
 // tcheck and print routines for state.ast/state.gr extensions
 
 #include "generic_aux.h"        // C++ AST, and genericPrintAmbiguities, etc.
+#if RICH
 #include "cc_env.h"             // Env
 #include "cc_print.h"           // olayer, PrintEnv
 #include "generic_amb.h"        // resolveAmbiguity, etc.
@@ -10,7 +11,13 @@
 #include <string.h>             // strcmp
 #include "datablok.h"
 #include "TargetInfo.h"
+#endif
 
 using namespace ellcc;
 using namespace std;
+
+// RICH: Dummies for now.
+void MR_state::tcheck(Env &env) { }
+void MR_state::print(PrintEnv &env) { }
+void MR_state::print(BPEnv &bp) const { }
 
