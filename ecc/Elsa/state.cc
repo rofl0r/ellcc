0c1b91a38464c98db2564cf546a1a044f4456afe
Richard Pennington <rich@ellcc.org>
2010-01-03 13:35:24 +0000

    More state machine changes.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@1643 82857169-6e38-49d7-aaa8-07fb558f12fc

--- ecc/Elsa/state.cc
+++ ecc/Elsa/state.cc
@@ -32,8 +32,32 @@
 using namespace ellcc;
 using namespace std;
 
+/** Type check a state definition.
+ * A state definition consists of a list of lists of StateEntries.
+ * The first list has one list for each AND state in the state.
+ * There may be, and often is, only one state list. if the state
+ * is not an AND state.
+ * The second list are the various entries to be performed in that state.
+ */
+static void checkState(Env& env, State* s)
+{
+    if (env.secondPassTcheck) {
+        FAKELIST_FOREACH(StateEntryList, s->states, state) {
+            FAKELIST_FOREACH_NC(StateEntry, state->entries, entry) {
+                // Type check each state entry.
+                entry->tcheck(env);
+            }
+        }
+    }
+}
+
+void MR_state::tcheck(Env &env)
+{
+    // Type check the state definition.
+    checkState(env, s);
+}
+
 // RICH: Dummies for now.
-void MR_state::tcheck(Env &env) { }
 void MR_state::print(PrintEnv &env) { }
 void MR_state::print(BPEnv &bp) const { }
 
@@ -49,8 +73,13 @@ void TF_state::itcheck(Env &env)
         env.report(s->loc, diag::err_state_global_initial_state)
             << s->d->getDeclaratorId()->getName()
             << SourceRange(s->loc, s->d->endloc);
+        s->initial = false;
     }
+
+    // Type check the state definition.
+    checkState(env, s);
 }
+
 void TF_state::print(PrintEnv &env) { }
 void TF_state::print(BPEnv &bp) const { }
 
@@ -61,9 +90,49 @@ void S_transition::icfg(CFGEnv &bp) { }
 void S_transition::cc2c(CC2CEnv &env) const { }
 void S_transition::cc2llvm(CC2LLVMEnv &env) const { }
 
-void SE_exit::tcheck(Env &env) { }
-void SE_reference::tcheck(Env &env) { }
-void SE_default::tcheck(Env &env) { }
-void SE_event::tcheck(Env &env) { }
-void SE_entry::tcheck(Env &env) { }
-void SE_timer::tcheck(Env &env) { }
+/** Type check a timeout definition.
+ */
+void SE_timer::tcheck(Env &env)
+{
+    e->tcheck(env, e);                          // Check the timeout expression.
+    s->itcheck(env);                            // Check the timeout statement.
+}
+
+/** Type check an entry action.
+ */
+void SE_entry::tcheck(Env &env)
+{
+    s->itcheck(env);                            // Check the entry statement.
+}
+
+/** Type check a event handler.
+ */
+void SE_event::tcheck(Env &env)
+{
+    // Get declarator info.
+    Declarator::Tcheck check(env.getSimpleType(ST_VOID), DF_NONE, DC_UNKNOWN);
+    d->tcheck(env, check);              // Check the event declarator.
+    s->itcheck(env);                    // Check the event handler statement.
+}
+
+/** Type check the default handler.
+ */
+void SE_default::tcheck(Env &env)
+{
+    s->itcheck(env);                            // Check the default statement.
+}
+
+/** Type check a nest state or state reference.
+ */
+void SE_reference::tcheck(Env &env)
+{
+    // Type check the state definition or reference.
+    checkState(env, s);
+}
+
+/** Type check an exit action.
+ */
+void SE_exit::tcheck(Env &env)
+{
+    s->itcheck(env);                            // Check the exit statement.
+}
