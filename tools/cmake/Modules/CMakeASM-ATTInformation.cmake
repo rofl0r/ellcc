ec2b8b32bd38a6665e7a55f7bd3f0dd5ec13caf8
Richard Pennington <rich@ellcc.org>
2010-11-27 17:42:20 +0000

    Cleanup before merge to trunk.
    git-svn-id: http://ellcc.org/svn/ellcc/branches/reset@2192 82857169-6e38-49d7-aaa8-07fb558f12fc

--- tools/cmake/Modules/CMakeASM-ATTInformation.cmake
+++
@@ -1,6 +0,0 @@
-# support for AT&T syntax assemblers, e.g. GNU as
-
-SET(ASM_DIALECT "-ATT")
-SET(CMAKE_ASM${ASM_DIALECT}_SOURCE_FILE_EXTENSIONS s;S;asm)
-INCLUDE(CMakeASMInformation)
-SET(ASM_DIALECT)
