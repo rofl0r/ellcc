458d3b3aee889ca2afd22aa5289d7fe9c5895b52
Richard Pennington <rich@ellcc.org>
2010-12-23 11:21:56 +0000

    Modifications for libecc.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2252 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/include.old/sys/dirent.h
+++
@@ -1,7 +0,0 @@
-/* <dirent.h> includes <sys/dirent.h>, which is this file.  On a
-   system which supports <dirent.h>, this file is overridden by
-   dirent.h in the libc/sys/.../sys directory.  On a system which does
-   not support <dirent.h>, we will get this file which uses #error to force
-   an error.  */
-
-#warning "<dirent.h> not supported"
--- libecc/include.old/syscall.h
+++
@@ -1,4 +0,0 @@
-#ifndef _SYSCALL_H_
-#define _SYSCALL_H_
-#include <sys/syscall.h>
-#endif  // _SYSCALL_H_
