a25c21cd738e9365bad1ab87a6c7e7bacb7ec923
Richard Pennington <rich@ellcc.org>
2010-12-23 12:14:15 +0000

    Modifications for libecc.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2253 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/src/linux/crt0.c
+++ libecc/src/linux/crt0.c
@@ -3,20 +3,25 @@
  * See start.h.
  */
 #include <stdlib.h>             // exit()
-#include <string.h>             // memset()
+// RICH: #include <string.h>             // memset()
 #include <stdarg.h>
 #include <asm/_start.h>         // _start()
 
-extern char **environ;
+static char *initial_env[] = { 0 };
+
+/* Posix says `environ' is a pointer to a null terminated list of pointers.
+   Hence `environ' itself is never NULL.  */
+char **environ = &initial_env[0];
+
 extern int main(int argc, char** argv, char** environ);
 
-extern char __bss_end__;
-extern char __bss_start__;
+// RICH: extern char __bss_end__;
+// RICH: extern char __bss_start__;
 
 void _estart(int argc, char** argv, char** _environ)
 {
     environ = _environ;
-    memset(&__bss_start__, 0, &__bss_end__ - &__bss_start__);
+// RICH:     memset(&__bss_start__, 0, &__bss_end__ - &__bss_start__);
     int status = main(argc, argv, environ);
     exit(status);
 }
