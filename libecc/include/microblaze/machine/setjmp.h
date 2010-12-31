395e080191f2edf61b4f31f38c3fa39c440e8f8c
Richard Pennington <rich@ellcc.org>
2010-12-31 21:22:25 +0000

    Modifications for libecc.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2294 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/include/microblaze/machine/setjmp.h
+++ libecc/include/microblaze/machine/setjmp.h
@@ -4,7 +4,7 @@
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
-#define	_JBLEN	64		/* size, in longs, of a jmp_buf */
+#define	_JBLEN	17		/* size, in longs, of a jmp_buf */
 
 /*
  * NOTE: The internal structure of a jmp_buf is *PRIVATE*
@@ -15,68 +15,52 @@
  * Description of the setjmp buffer
  *
  * word  0	magic number	(dependant on creator)
- *       1 -  3	f4		fp register 4
- *	 4 -  6	f5		fp register 5
- *	 7 -  9 f6		fp register 6
- *	10 - 12	f7		fp register 7
- *	13	fpsr		fp status register
- *	14	r4		register 4
- *	15	r5		register 5
- *	16	r6		register 6
- *	17	r7		register 7
- *	18	r8		register 8
- *	19	r9		register 9
- *	20	r10		register 10 (sl)
- *	21	r11		register 11 (fp)
- *	22	r12		register 12 (ip)
- *	23	r13		register 13 (sp)
- *	24	r14		register 14 (lr)
- *	25	signal mask	(dependant on magic)
- *	26	(con't)
- *	27	(con't)
- *	28	(con't)
+ *       1     	r1		register 1 (sp)
+ *	 2     	r15		register 15 (lr)
+ *	 3      r18		register 18
+ *       4      r19             register 19
+ *       5      r20             register 20
+ *       6      r21             register 21
+ *       7      r22             register 22
+ *       8      r23             register 23
+ *       9      r24             register 24
+ *      10      r25             register 25
+ *      11      r26             register 26
+ *      12      r27             register 27
+ *      13      r28             register 28
+ *      14      r29             register 29
+ *      15      r30             register 30
+ *      16      signal mask
  *
  * The magic number number identifies the jmp_buf and
  * how the buffer was created as well as providing
  * a sanity check
- *
- * A side note I should mention - Please do not tamper
- * with the floating point fields. While they are
- * always saved and restored at the moment this cannot
- * be garenteed especially if the compiler happens
- * to be generating soft-float code so no fp
- * registers will be used.
- *
- * Whilst this can be seen an encouraging people to
- * use the setjmp buffer in this way I think that it
- * is for the best then if changes occur compiles will
- * break rather than just having new builds falling over
- * mysteriously.
  */
 
-#define _JB_MAGIC__SETJMP	0x4278f500
-#define _JB_MAGIC_SETJMP	0x4278f501
+#define _JB_MAGIC_SETJMP	0x4278f500
+#define _JB_MAGIC_MASK	        0xffffff00
+#define _JB_MAGIC_BITS	        0x000000ff
+#define _JB_MAGIC_SIGMASK	0x01
 
 /* Valid for all jmp_buf's */
 
 #define _JB_MAGIC		 0
-#define _JB_REG_F4		 1
-#define _JB_REG_F5		 4
-#define _JB_REG_F6		 7
-#define _JB_REG_F7		10
-#define _JB_REG_FPSR		13
-#define _JB_REG_R4		14
-#define _JB_REG_R5		15
-#define _JB_REG_R6		16
-#define _JB_REG_R7		17
-#define _JB_REG_R8		18
-#define _JB_REG_R9		19
-#define _JB_REG_R10		20
-#define _JB_REG_R11		21
-#define _JB_REG_R12		22
-#define _JB_REG_R13		23
-#define _JB_REG_R14		24
+#define _JB_REG_R1		 1
+#define _JB_REG_R15		 2
+#define _JB_REG_R18		 3
+#define _JB_REG_R19		 4
+#define _JB_REG_R20		 5
+#define _JB_REG_R21		 6
+#define _JB_REG_R22		 7
+#define _JB_REG_R23		 8
+#define _JB_REG_R24		 9
+#define _JB_REG_R25		10
+#define _JB_REG_R26		11
+#define _JB_REG_R27		12
+#define _JB_REG_R28		13
+#define _JB_REG_R29		14
+#define _JB_REG_R30		15
 
-/* Only valid with the _JB_MAGIC_SETJMP magic */
+/* Only valid with the _JB_MAGIC_SETJMP bit */
 
-#define _JB_SIGMASK		25
+#define _JB_SIGMASK		16
