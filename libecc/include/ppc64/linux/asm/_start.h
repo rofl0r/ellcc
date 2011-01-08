1e2a6366cca947b6df650dc36ca2838e9894d65e
Richard Pennington <rich@ellcc.org>
2011-01-08 22:10:39 +0000

    Power64 now starts.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2344 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/include/ppc64/linux/asm/_start.h
+++ libecc/include/ppc64/linux/asm/_start.h
@@ -5,19 +5,19 @@ asm(
 "       .align  3\n"
 "       .globl  _start\n"
 "_start:\n"
-"       .quad   ._start, .TOC.@tocbase, 0\n"
+"       .quad   __start, .TOC.@tocbase, 0\n"
 "       .size _start, 24\n"
 "       .section \".text\"\n"
-"       .type   ._start, %function\n"
+"       .type   __start, %function\n"
 "       .align  3\n"
-"._start:\n"
+"__start:\n"
 "       mr      9, 1\n"                 // Save the original stack pointer.
 "       clrrdi  1, 1, 4\n"              // Align the stack  to 16 bytes. */
 "       li      0, 0\n"                 // Clear the frame pointer.
-"       stdu    1, -128(1)\n"           // The initial stack frame. RICH?
+"       stdu    1, -128(1)\n"           // The initial stack frame.
 "       mtlr    0\n"                    // Clear the link register.
 "       std     0, 0(1)\n"              // And save it.
-"       lwz     3, 0(9)\n"              // Get argc...
+"       ld      3, 0(9)\n"              // Get argc...
 "       addi    4, 9, 8\n"              // ... and argv ...
 "       addi    5, 3, 1\n"              // ... and argc + 1 ...
 "       add     5, 5, 5\n"              // ... (argc + 1) * 2
@@ -26,6 +26,6 @@ asm(
 "       add     5, 5, 4\n"              // ... indexed from argv.
 "       b       _estart\n"              // Let's go!
 "       nop\n"
-"       .size   ._start, .-._start\n"
+"       .size   __start, .-__start\n"
 );
 
