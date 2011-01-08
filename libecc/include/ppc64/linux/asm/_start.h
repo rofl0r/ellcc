b396cf9c70e15a191047ba37ae80a89516ef4a8a
Richard Pennington <rich@ellcc.org>
2011-01-08 18:52:33 +0000

    Power64 now starts.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2339 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/include/ppc64/linux/asm/_start.h
+++ libecc/include/ppc64/linux/asm/_start.h
@@ -1,17 +1,22 @@
 /** The initial entry point.
  */
 asm(
-"       .text\n"
-"       .globl  _start\n"
-"       .type   _start, %function\n"
+"       .section \".opd\",\"aw\"\n"
+"       .align  3\n"
 "_start:\n"
+"       .quad   ._start, .TOC.@tocbase, 0\n"
+"       .size _start, 24\n"
+"       .section \".text\"\n"
+"       .globl  ._start\n"
+"       .type   ._start, %function\n"
+"       .align  3\n"
+"._start:\n"
 "       mr      9, 1\n"                 // Save the original stack pointer.
-/* RICH: qemu clears upper 32 bits.
-"       clrrwi  1, 1, 4\n"              // Align the stack  to 16 bytes. */
+"       clrrdi  1, 1, 4\n"              // Align the stack  to 16 bytes. */
 "       li      0, 0\n"                 // Clear the frame pointer.
-"       stwu    1, -16(1)\n"            // The initial stack frame. RICH?
+"       stdu    1, -128(1)\n"           // The initial stack frame. RICH?
 "       mtlr    0\n"                    // Clear the link register.
-"       stw     0, 0(1)\n"              // And save it.
+"       std     0, 0(1)\n"              // And save it.
 "       lwz     3, 0(9)\n"              // Get argc...
 "       addi    4, 9, 8\n"              // ... and argv ...
 "       addi    5, 3, 1\n"              // ... and argc + 1 ...
@@ -20,6 +25,7 @@ asm(
 "       add     5, 5, 5\n"              // ... (argc + 1) * 8 to compute environ.
 "       add     5, 5, 4\n"              // ... indexed from argv.
 "       b       _estart\n"              // Let's go!
-"       .size   _start, .-_start\n"
+"       nop\n"
+"       .size   ._start, .-._start\n"
 );
 
