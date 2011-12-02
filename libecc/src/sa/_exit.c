3d9081fd8fc1796d4be8c246d5e3a81cb0d369f8
Richard Pennington <rich@ellcc.org>
2011-12-02 12:21:33 +0000

    Start of stand alone libraries.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2582 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/src/sa/_exit.c
+++
@@ -1,9 +0,0 @@
-#include <syscall.h>
-#include <abort-instr.h>
-
-void _exit(int status)
-{
-    // Get rid of the compiler warning about noreturn functions returning.
-    for ( ;; ) {
-    }
-}
--- libecc/src/sa/write.c
+++
@@ -1,7 +0,0 @@
-#include <syscall.h>
-#include <unistd.h>
-
-ssize_t write(int fd, const void* buf, size_t count)
-{
-    return 0;
-}
