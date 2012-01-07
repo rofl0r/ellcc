3affc9ed96ee0f79e8cd069f26bbf44a1f80eabd
Richard Pennington <rich@ellcc.org>
2012-01-07 20:25:54 +0000

    Started tests for stdio.h.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2706 82857169-6e38-49d7-aaa8-07fb558f12fc

---
+++ test/src/libs/gen/001stdio.h
@@ -0,0 +1,32 @@
+#include <ecc_test.h>
+#include <stdio.h>
+
+TEST_GROUP(Stdio)
+    TEST_TRACE(C99 7.19.1/2)
+    size_t size;
+    FILE *f;
+    fpos_t fpos;
+    int i;
+    char *p;
+    TEST_TRACE(C99 7.19.1/3)
+    p = NULL;
+    TEST(_IOFBF != _IOLBF && _IOFBF != _IONBF,
+         "_IOFBF has a distinct value");
+    TEST(_IOLBF != _IONBF,
+         "_IOLBF has a distinct value");
+    TEST(BUFSIZ >= 256, "BUFSIZE is at least 256")
+    TEST(EOF < 0, "EOF is a negative value");
+    TEST(FOPEN_MAX >= 8, "FOPEN_MAX is at least 8")
+    i = FILENAME_MAX;
+    i = L_tmpnam;
+    TEST(SEEK_CUR != SEEK_END && SEEK_CUR != SEEK_SET,
+         "SEEK_CUR has a distinct value");
+    TEST(SEEK_END != SEEK_SET,
+         "SEEK_END has a distinct value");
+    TEST(TMP_MAX >= 25, "TMP_MAX is at least 25")
+    f = stderr;
+    f = stdin;
+    f = stdout;
+    TEST_TRACE(C99 7.19.4.1)
+END_GROUP
+
