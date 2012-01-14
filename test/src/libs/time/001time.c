2d94686b06dc1344fe514c91458d61850c5bd3af
Richard Pennington <rich@ellcc.org>
2012-01-14 19:14:34 +0000

    Tests of time.h.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2739 82857169-6e38-49d7-aaa8-07fb558f12fc

--- test/src/libs/time/001time.c
+++ test/src/libs/time/001time.c
@@ -2,5 +2,23 @@
 #include <time.h>
 
 TEST_GROUP(Time)
-    TEST_TRACE(C99 7.23)
+    char *p;
+    TEST_TRACE(C99 7.23/3)
+    size_t size;
+    clock_t c;
+    time_t t;
+    struct tm tm;
+    TEST_TRACE(C99 7.23/2)
+    p = NULL;
+    c = CLOCKS_PER_SEC;
+    TEST_TRACE(C99 7.23/4)
+    tm.tm_sec = 59;
+    tm.tm_min = 9;
+    tm.tm_hour = 13;
+    tm.tm_mday = 14;
+    tm.tm_mon = 0;
+    tm.tm_year = 112;
+    tm.tm_wday = 6;
+    tm.tm_yday = 11;
+    tm.tm_isdst = 1;
 END_GROUP
