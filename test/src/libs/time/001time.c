89b6686802968914c22e862b05a180307912b28d
Richard Pennington <rich@ellcc.org>
2012-01-15 12:33:14 +0000

    Tests of time.h.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2742 82857169-6e38-49d7-aaa8-07fb558f12fc

--- test/src/libs/time/001time.c
+++ test/src/libs/time/001time.c
@@ -18,10 +18,10 @@ TEST_GROUP(Time)
         .tm_hour = 13,
         .tm_mday = 14,
         .tm_mon = 0,
-        .tm_year = 112,
+        .tm_year = 2012 - 1900,
         .tm_wday = 6,
         .tm_yday = 11,
-        .tm_isdst = 1,
+        .tm_isdst = 0,
     };
     TEST_TRACE(C99 7.23.2.1)
     c = clock();
@@ -31,4 +31,15 @@ TEST_GROUP(Time)
     TEST_EXCLUDE(PPC64, "http://ellcc.org/bugzilla/show_bug.cgi?id=31")
     TEST_EXCLUDE(PPC, "http://ellcc.org/bugzilla/show_bug.cgi?id=32")
         t = mktime(&tm);
+    TEST_TRACE(C99 7.23.2.3)
+    TEST(time(&t) == t, "time() directly and indirectly returns the time");
+    TEST_TRACE(C99 7.23.2.2)
+    TEST(difftime(t, t) == 0.0, "difftime() with identical times is 0.0");
+    TEST_TRACE(C99 7.23.3.1)
+    TEST_EXCLUDE(MICROBLAZE, "http://ellcc.org/bugzilla/show_bug.cgi?id=33")
+    TEST_EXCLUDE(PPC64, "http://ellcc.org/bugzilla/show_bug.cgi?id=34")
+    TEST_EXCLUDE(PPC, "http://ellcc.org/bugzilla/show_bug.cgi?id=35") {
+        p = asctime(&ctm);
+        TEST(strcmp(p, "Sat Jan 14 13:09:59 2012\n") == 0, "asctime() succeeds");
+    }
 END_GROUP
