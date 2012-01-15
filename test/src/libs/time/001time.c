9974f27ad298becf334f9d4e47c9557c7440f7f0
Richard Pennington <rich@ellcc.org>
2012-01-15 13:38:14 +0000

    Tests of time.h.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2744 82857169-6e38-49d7-aaa8-07fb558f12fc

--- test/src/libs/time/001time.c
+++ test/src/libs/time/001time.c
@@ -8,6 +8,8 @@ TEST_GROUP(Time)
     clock_t c;
     time_t t;
     struct tm tm;
+    struct tm *rtm;
+    char buffer[100];
     TEST_TRACE(C99 7.23/2)
     p = NULL;
     c = CLOCKS_PER_SEC;
@@ -29,10 +31,11 @@ TEST_GROUP(Time)
     tm = ctm;
     TEST_EXCLUDE(MICROBLAZE, "http://ellcc.org/bugzilla/show_bug.cgi?id=30")
     TEST_EXCLUDE(PPC64, "http://ellcc.org/bugzilla/show_bug.cgi?id=31")
-    TEST_EXCLUDE(PPC, "http://ellcc.org/bugzilla/show_bug.cgi?id=32") {
+    TEST_EXCLUDE(PPC, "http://ellcc.org/bugzilla/show_bug.cgi?id=32")
+    {
         t = mktime(&tm);
         TEST_TRACE(C99 7.23.3.4)
-        struct tm *rtm = localtime(&t); // NOTE: mktime() assumes tm is in local time.
+        rtm = localtime(&t);    // NOTE: mktime() assumes tm is in local time.
         TEST(rtm != NULL, "localtime() returns a broken-down time");
         TEST(rtm->tm_sec == ctm.tm_sec, "seconds match");
         TEST(rtm->tm_min == ctm.tm_min, "minutes match");
@@ -43,6 +46,10 @@ TEST_GROUP(Time)
         TEST(rtm->tm_wday == ctm.tm_wday, "week days match");
         TEST(rtm->tm_yday == ctm.tm_yday, "day of year matches");
         TEST(rtm->tm_isdst == ctm.tm_isdst, "daylight savings time matches");
+        TEST_TRACE(C99 7.23.3.2)
+        p = ctime(&t);
+        TEST(strncmp(p, "Sat Jan 14 13:09:59 2012\n", 11) == 0, "ctime(0) succeeds");
+        TEST(strcmp(&p[13], ":09:59 2012\n") == 0, "ctime(0) succeeds");
     }
     TEST_TRACE(C99 7.23.2.3)
     TEST(time(&t) == t, "time() directly and indirectly returns the time");
@@ -51,8 +58,17 @@ TEST_GROUP(Time)
     TEST_TRACE(C99 7.23.3.1)
     TEST_EXCLUDE(MICROBLAZE, "http://ellcc.org/bugzilla/show_bug.cgi?id=33")
     TEST_EXCLUDE(PPC64, "http://ellcc.org/bugzilla/show_bug.cgi?id=34")
-    TEST_EXCLUDE(PPC, "http://ellcc.org/bugzilla/show_bug.cgi?id=35") {
+    TEST_EXCLUDE(PPC, "http://ellcc.org/bugzilla/show_bug.cgi?id=35")
+    {
         p = asctime(&ctm);
         TEST(strcmp(p, "Sat Jan 14 13:09:59 2012\n") == 0, "asctime() succeeds");
+        TEST_TRACE(C99 7.23.3.3)
+        t = 0;
+        rtm = gmtime(&t);
+        p = asctime(rtm);
+        TEST(strcmp(p, "Thu Jan  1 00:00:00 1970\n") == 0, "asctime(gmtime(0)) succeeds");
+        size = strftime(buffer, sizeof(buffer), "format", &ctm);
+        TEST(size == 6, "strftime() returns the number of bytes written");
+        TEST(strcmp(buffer, "format") == 0, "strftime() copies non-format characters");
     }
 END_GROUP
