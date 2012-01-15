360a3ca433ec91b971007f6bde0da7886c2b47f9
Richard Pennington <rich@ellcc.org>
2012-01-15 14:18:13 +0000

    Tests of wchar.h.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2746 82857169-6e38-49d7-aaa8-07fb558f12fc

--- test/src/libs/wchar/001wchar.c
+++ test/src/libs/wchar/001wchar.c
@@ -2,10 +2,17 @@
 #include <wchar.h>
 
 TEST_GROUP(Wchar)
+    FILE *f;
     TEST_TRACE(C99 7.24/2)
     wchar_t wchar;
     size_t size;
     mbstate_t mbstate;
     wint_t wint;
     struct tm *tm;
+    TEST_TRACE(C99 7.24/3)
+    f = NULL;
+    wint = WCHAR_MIN;
+    wint = WCHAR_MAX;
+    wint = WEOF;
+    TEST_TRACE(C99 7.242.1)
 END_GROUP
