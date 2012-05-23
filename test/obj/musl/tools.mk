1c488f9a6cd0aaba4974b91cf24b9f7785fd4097
Richard Pennington <rich@ellcc.org>
2012-05-23 11:39:16 +0000

    Added a musl build.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@2926 82857169-6e38-49d7-aaa8-07fb558f12fc

---
+++ test/obj/musl/tools.mk
@@ -0,0 +1,15 @@
+# This file can be configured to compile libecc with an external compiler.
+# The name of the external compiler.
+ECC  := $(shell cd ../../../../..; pwd)
+XCC = $(ECC)/bin/x86_64-linux-ecc
+# ARCH should be one of arm, i386, microblaze, mips, nios2, ppc64, ppc, sparc, or x86_64.
+ARCH := $(shell uname -m)
+XCFLAGS = -std=gnu99 -nostdinc
+# Get compiler defined headers (this assumes XCC has gcc compatable command line options).
+XCFLAGS += -I$(ECC)/test/include
+DOFF := $(ECC)/libecc
+XCFLAGS += -I$(ECC)/musl-build/x86_64/include
+OSLIBDIR := $(ECC)/musl-build/x86_64/lib
+XLDFLAGS += -static -nostdlib $(OSLIBDIR)/crt1.o $(OSLIBDIR)/crti.o
+XLDEXTRA = -L$(OSLIBDIR) $(OSLIBDIR)/crtn.o -lm -lc -L$(ECC)/libecc/lib/x86_64 -lcompiler-rt
+EXE = ./
